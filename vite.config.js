50210b0b8c4e584d623dea2cae8b2e997e33b47a
Mohmmed Wal <ms56@smail.ucas.edu.ps>
2026-09-18 17:00:25 +0200

    switch css minify to esbuild for vercel

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    cssMinify: 'esbuild', // إجبار Vite على استخدام esbuild بدلاً من lightningcss
+  },
   optimizeDeps: {
     include: ['firebase/app', 'firebase/firestore', 'firebase/auth']
-  },
-  build: {
-    commonjsOptions: {
-      include: [/firebase/, /node_modules/]
-    }
   }
 })
