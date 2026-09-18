c1ba0375cf1d269accd5be9168901d4f4f566fe2
Mohmmed Wal <ms56@smail.ucas.edu.ps>
2026-09-18 17:04:49 +0200

    downgrade vite to stable version 5

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    cssMinify: 'esbuild', // إجبار Vite على استخدام esbuild بدلاً من lightningcss
-  },
-  optimizeDeps: {
-    include: ['firebase/app', 'firebase/firestore', 'firebase/auth']
-  }
 })
